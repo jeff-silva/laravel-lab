bab890c99ef96bf43f18190a6cc714d090a58137
Jeferson Silva <jeferson.i.silva@gmail.com>
2024-07-17 18:55:02 -0300

    supervisor

--- backend/.docker/start-container.sh
+++ backend/.docker/start-container.sh
@@ -10,4 +10,9 @@ cd /app
 composer install
 php artisan config:clear
 
+echo "" > /app/storage/logs/supervisor-php-stdout.log
+echo "" > /app/storage/logs/supervisor-php-stderr.log
+echo "" > /app/storage/logs/supervisor-cron-stdout.log
+echo "" > /app/storage/logs/supervisor-cron-stderr.log
+
 exec supervisord -c /etc/supervisor/supervisord.conf
